31364a21d6c58f0a1d7ee21a24b4df289a4fd767
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2019-10-22 10:39:13 +0300

    Implement units module

--- JavaScript/3-units.js
+++ JavaScript/3-units.js
@@ -1,19 +1,73 @@
 'use strict';
 
+const DURATION_UNITS = {
+  days:    { rx: /(\d+)\s*d/, mul: 86400 },
+  hours:   { rx: /(\d+)\s*h/, mul: 3600 },
+  minutes: { rx: /(\d+)\s*m/, mul: 60 },
+  seconds: { rx: /(\d+)\s*s/, mul: 1 }
+};
+
 // Parse duration string to seconds
 // Example: duration('1d 10h 7m 13s')
 const duration = s => {
-  // return Number
+  if (typeof(s) === 'number') return s;
+  let result = 0;
+  let unit, match, key;
+  if (typeof(s) === 'string') {
+    for (key in DURATION_UNITS) {
+      unit = DURATION_UNITS[key];
+      match = s.match(unit.rx);
+      if (match) result += parseInt(match[1], 10) * unit.mul;
+    }
+  }
+  return result * 1000;
 };
 
 // Convert bytes: Number to size: String with nearest units: Kb, Mb, Gb...
 const bytesToSize = bytes => {
-  // return String
+  if (bytes === 0) return '0';
+  const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1000)), 10);
+  return (
+    Math.round(bytes / Math.pow(1000, i), 2) +
+    bytesToSize.sizes[i]
+  );
 };
+
+bytesToSize.sizes = [
+  '', ' Kb', ' Mb', ' Gb', ' Tb', ' Pb', ' Eb', ' Zb', ' Yb'
+];
 
 // Parse units and convert size: String to bytes: Number
 const sizeToBytes = size => {
-  // return Number
+  if (typeof(size) === 'number') return size;
+  size = size.toUpperCase();
+  let result = 0;
+  const units = sizeToBytes.units;
+  if (typeof(size) === 'string') {
+    let key, unit, match;
+    let found = false;
+    for (key in units) {
+      unit = units[key];
+      match = size.match(unit.rx);
+      if (match) {
+        result += parseInt(match[1], 10) * Math.pow(10, unit.pow);
+        found = true;
+      }
+    }
+    if (!found) result = parseInt(size, 10);
+  }
+  return result;
+};
+
+sizeToBytes.units = {
+  yb: { rx: /(\d+)\s*YB/, pow: 24 },
+  zb: { rx: /(\d+)\s*ZB/, pow: 21 },
+  eb: { rx: /(\d+)\s*EB/, pow: 18 },
+  pb: { rx: /(\d+)\s*PB/, pow: 15 },
+  tb: { rx: /(\d+)\s*TB/, pow: 12 },
+  gb: { rx: /(\d+)\s*GB/, pow: 9 },
+  mb: { rx: /(\d+)\s*MB/, pow: 6 },
+  kb: { rx: /(\d+)\s*KB/, pow: 3 }
 };
 
 // Tests
